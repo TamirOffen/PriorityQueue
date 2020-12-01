dd865f4bd445170e4f7d5192101f3e2e1f7d9e2e
Nabeeh Abusaleh <nabeeh@campus.technion.ac.il>
2020-12-01 13:25:14 +0200

    Implemented the struct

--- priority_queue.c
+++ priority_queue.c
@@ -1,3 +1,22 @@
+#include <stdio.h>
 #include "priority_queue.h"
 
-//TODO: fill in 
+
+struct PriorityQueue_t {
+
+PQElement* elements;
+PQElementPriority* priorities;
+
+int size;
+int maxsize;
+int iterator;
+
+CopyPQElement copy_element;
+FreePQElement free_element;
+EqualPQElements compare_elements;
+
+CopyPQElementPriority copy_priority;
+FreePQElementPriority free_priority;
+ComparePQElementPriorities compare_priorities;
+
+};
