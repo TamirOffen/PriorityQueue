3cd56d87568d45b59d8144f6a90fa0581e06f1fd
Tamir Offen <tamiroffen@Tamirs-MacBook-Pro.local>
2020-12-03 13:36:25 +0200

    added new implementation

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
 #include "priority_queue.h"
 #include <stdio.h>
 
+static PQElementPriority copyIntGeneric(PQElementPriority n) {
+    if (!n) {
+        return NULL;
+    }
+    int *copy = malloc(sizeof(*copy));
+    if (!copy) {
+        return NULL;
+    }
+    *copy = *(int *) n;
+    return copy;
+}
+static void freeIntGeneric(PQElementPriority n) {
+    free(n);
+}
+static int compareIntsGeneric(PQElementPriority n1, PQElementPriority n2) {
+    return (*(int *) n1 - *(int *) n2);
+}
+
+
+
+
+
+
 int main() {
+
+    // PriorityQueue pq = pqCreate();
 
     printf("test\n");
 
--- priority_queue.c
+++ priority_queue.c
@@ -8,31 +8,31 @@
                          Implementation constants
  ----------------------------------------------------------------------*/
 
+// Returned by find() when a value is not in the set
+#define ELEMENT_NOT_FOUND -1
 
 #define INITIAL_SIZE 10
 
 typedef struct ElementsStruct {
     PQElement element;
     PQElementPriority priority; 
-}Element;                                  
+} Element;                                  
 
 
 struct PriorityQueue_t {
+    Element* list_of_elements;                 
+    Element iterator;
 
-Element* list_of_elements;                 
-Element* iterator;
+    int size;
+    int max_size;
 
-int size;
-int max_size;
+    CopyPQElement copy_element;
+    FreePQElement free_element;
+    EqualPQElements compare_elements;
 
-CopyPQElement copy_element;
-FreePQElement free_element;
-EqualPQElements compare_elements;
-
-CopyPQElementPriority copy_priority;
-FreePQElementPriority free_priority;
-ComparePQElementPriorities compare_priorities;
-
+    CopyPQElementPriority copy_priority;
+    FreePQElementPriority free_priority;
+    ComparePQElementPriorities compare_priorities;
 };
 
 
@@ -40,7 +40,7 @@ ComparePQElementPriorities compare_priorities;
                              Static helper functions
  ----------------------------------------------------------------------*/
 
-//Returns the index of the same element as the recieved element with the highest priority.
+// Returns the index of the same element as the recieved element with the highest priority.
 static int findSpecificElement(PriorityQueue queue, PQElement element)
 {
     assert(queue != NULL);
@@ -65,16 +65,22 @@ static int findSpecificElement(PriorityQueue queue, PQElement element)
     }
 
     if(max_priority_index == -1){
-    return PQ_ELEMENT_DOES_NOT_EXISTS;
+        return ELEMENT_NOT_FOUND;
     }
 
     return max_priority_index;
 }
 
-//Returns the index of the highest priority element.
+// Returns the index of the highest priority element.
+// If there are multiple elements with the high priority, than return the first one in list_of_elements
 static int findHighestPriorityElement(PriorityQueue queue) 
 {
     assert(queue != NULL);
+    
+    //    1 2 3 3 2 2
+        // 2 for loops:
+        //   1st: get you the highest priority (in the exmample, return 3)
+        //   2nd: return the index of the first element with this highest priority
 
     int max_priority_index = 0;
 
@@ -106,14 +112,15 @@ PriorityQueue pqCreate(CopyPQElement copy_element,
                        EqualPQElements equal_elements,
                        CopyPQElementPriority copy_priority,
                        FreePQElementPriority free_priority,
-                       ComparePQElementPriorities compare_priorities)
-{
-    assert(copy_element != NULL && 
-           free_element != NULL && 
-           equal_elements != NULL && 
-           copy_priority != NULL && 
-           free_priority != NULL && 
-           compare_priorities != NULL);
+                       ComparePQElementPriorities compare_priorities) {
+    if(copy_element == NULL || 
+           free_element == NULL || 
+           equal_elements == NULL || 
+           copy_priority == NULL || 
+           free_priority == NULL || 
+           compare_priorities == NULL) {
+               return NULL;
+           }
 
 
     PriorityQueue queue = malloc (sizeof(*queue));
@@ -124,27 +131,33 @@ PriorityQueue pqCreate(CopyPQElement copy_element,
 
     //Add allocate elements struct
     queue->list_of_elements = malloc(INITIAL_SIZE * sizeof(Element));
-    
-
-
-    queue->priorities = malloc(INITIAL_SIZE * sizeof(PQElementPriority));
-    if(queue->priorities == NULL)
-    {
-        pqDestroy(queue);
+    if(queue->list_of_elements == NULL) {
+        free(queue);
         return NULL;
     }
-    //
 
+    queue->list_of_elements[0].element = malloc(sizeof(PQElement));
+    if(queue->list_of_elements[0].element == NULL) {
+        free(queue->list_of_elements);
+        free(queue);
+        return NULL;
+    }
+
+    queue->list_of_elements[0].priority = malloc(sizeof(PQElementPriority));
+    if(queue->list_of_elements[0].priority == NULL) {
+        free(queue->list_of_elements[0].element);
+        free(queue->list_of_elements);
+        free(queue);
+        return NULL;
+    }
+ 
     queue->size = 0;
     queue->max_size = INITIAL_SIZE;
-    //NULL
-    queue->iterator = 0;
-    //
-
+    queue->iterator = NULL;
+    
     queue->copy_element = copy_element;
     queue->free_element = free_element;
     queue->compare_elements = equal_elements;
-
 
     queue->copy_priority = copy_priority;
     queue->free_priority = free_priority;
@@ -152,23 +165,22 @@ PriorityQueue pqCreate(CopyPQElement copy_element,
 }
 
 
-void pqDestroy (PriorityQueue queue)
-{
-    if(queue == NULL)
-    {
-        return;
-    }
+// void pqDestroy (PriorityQueue queue)
+// {
+//     if(queue == NULL) {
+//         return;
+//     }
 
-    while(!pqIsEmpty(queue))
-    {
-        pqRemoveElement(queue, pqGetFirst(queue));
-    }
-
-    //add free elements struct func instead
-    free(queue->elements);
-    free(queue->priorities);
-    free(queue);
-}
+//     while(!pqIsEmpty(queue))
+//     {
+//         pqRemoveElement(queue, pqGetFirst(queue));
+//     }
+//     
+//     //add free elements struct func instead
+//     free(queue->list_of_elements);
+//     free(queue->iterator);
+//     free(queue);
+// }
 
 
 /*----------------------------------------------------------------------
@@ -177,45 +189,54 @@ void pqDestroy (PriorityQueue queue)
 
 
 
-PriorityQueueResult pqRemoveElement (PriorityQueue queue, PQElement element)
-{
-    assert(queue != NULL);
+// PriorityQueueResult pqRemoveElement (PriorityQueue queue, PQElement element)
+// {
+//     assert(queue != NULL);
 
-    int index = findSpecificElement(queue, element);
-    if (index == PQ_ELEMENT_DOES_NOT_EXISTS) {
-        return PQ_ERROR;
-    }
+//     int index = findSpecificElement(queue, element);
+//     if (index == PQ_ELEMENT_DOES_NOT_EXISTS) {
+//         return PQ_ERROR;
+//     }
 
-    queue->free_element(queue->elements[index]);
-    queue->elements[index] = queue->elements[queue->size - 1];
+//     queue->free_element(queue->elements[index]);
+//     queue->elements[index] = queue->elements[queue->size - 1];
 
-    queue->free_priority(queue->priorities[index]);
-    queue->priorities[index] = queue->priorities[queue->size - 1];
+//     queue->free_priority(queue->priorities[index]);
+//     queue->priorities[index] = queue->priorities[queue->size - 1];
 
-    queue->size--;
-    queue->iterator = 0;
+//     queue->size--;
+//     queue->iterator = 0;
 
-    return PQ_SUCCESS;
-}
-
-
-int pqGetSize(PriorityQueue queue) 
-{
-    assert(queue != NULL); 
-    return queue->size;
-}
+//     return PQ_SUCCESS;
+// }
 
 
-/*----------------------------------------------------------------------
-                               Queue iteration
- ----------------------------------------------------------------------*/
+// int pqGetSize(PriorityQueue queue) 
+// {
+//     assert(queue != NULL); 
+//     return queue->size;
+// }
+
+
+// /*----------------------------------------------------------------------
+//                                Queue iteration
+//  ----------------------------------------------------------------------*/
 
 
 PQElement pqGetFirst(PriorityQueue queue) 
 {
-    assert(queue != NULL);
-    queue->iterator = 0;
-    return (queue->elements[findHighestPriorityElement(queue)]);
+    if(queue == NULL) {
+        return NULL;
+    }
+
+    int highest_priority_index = findHighestPriorityElement(queue);
+    if(highest_priority_index != ELEMENT_NOT_FOUND) {
+        return NULL;
+    }
+    
+    queue->iterator = queue->list_of_elements[highest_priority_index];
+
+    return queue->list_of_elements[highest_priority_index].element;
 }
 
 
